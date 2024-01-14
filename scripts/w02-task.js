5dccdaab626c1fbdc993071115846796245b7db4
Tsitsimutsvedu <mutsvedutsitsi@gmail.com>
2024-01-14 21:36:57 +0200

    test.html

---
+++ scripts/w02-task.js
@@ -0,0 +1,29 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+let fullName = "Tsitsi Mutsvedu";
+
+
+/* Step 3 - Element Variables */
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+
+/* Step 4 - Adding Content */
+let profilePicture = "images/example.jpj";
+
+
+/* Step 5 - Array */
+const nameElement = document.getElementById("name");
+
+const yearElement = document.querySelector("#year");
+
+const fullName = "Tsitsi Mutsvedu";
+nameElement.innerHTML = "<strong>${fullName}<strong>";
+
+
+
+
+
+
+
