622ac0d54670970e72c6f639280de35c4f20a758
Tsitsimutsvedu <mutsvedutsitsi@gmail.com>
2024-02-03 09:41:14 +0200

    w03 task.js

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -2,58 +2,92 @@
 
 /* FUNCTIONS */
 /* Function Definition - Add Numbers */
-function add (number1 , number2) {
+function add (number1 , number2){
     return number1 + number2;
 }
-function addNumbers() {}
-    var number1 = document.getElementById("add1").value;
-    var number2 = document.getElementById("add2").value;
-     if (!isNaN(number1) && !isNaN(number2)) {
-        var result = parseFloat(number1) + parseFloat(number2);
-        console.log("The sum is: " + result);
-     }
-     else{
-        console.log("Please enter valid numbers.");
-     }
+let addNumber1 = Number(document.querySelector('#add1').value);
+let addNumber2 = Number(document.querySelector('#add2').value);
 
-     function addNumbers(){
-        var number1 = document.getElementById("add1").value;
-        var number2 = document.getElementById("add2").value;
-        if (!isNaN(number1) && !isNaN(number2)) {
-            var sum = add(parseFloat(number1) , parseFloat(number2));
-            document.getElementById("sum").value = sum;
-            
-        } else {
+document.querySelector('#sum').value = add(addNumber1, addNumber2);
 
-        console.log("Please enter valid numbers.");
+document.querySelector('#addNumbers').addEventListener ('click' , addNumbers);
 
-    document.getElementById("addnumbers").addEventListener("click", addNumbers);
-    
-
-     }
-}
-
+     
 /* Function Expression - Subtract Numbers */
+function Subtract (number1 , number2){
+    return number1 - number2;
+}
+let subtractNumber1 = Number(document.querySelector('#subtract1').value);
+let subtractNumber2 = Number(document.querySelector('#subtract2').value);
+
+document.querySelector('#difference').value = subtract(subtractNumber1, subtractNumber2);
+
+document.querySelector('#subtractNumbers').addEventListener('click' , subtractNumbers);
 
 
-/* Arrow Function - Multiply Numbers */
+    /* Arrow Function - Multiply Numbers */
+const multiply = (a,b) => a*b;
 
+const multiplyNumbers = ()=>{
+    const factor1 = parseFloat (document.getElementById('factor1').value);
+    const factor2 = parseFloat (document.getElementById('factor2').value);
+    const result = multiply(factor1, factor2);
+    document.getElementById('product').innerText = 'result: ${result}';
+}
+document.getElementById('multiplyNumbers').addEventListener('click, multiplyNumbers');
 
 /* Open Function Use - Divide Numbers */
+const divide = (x , y) => x / y;
+
+const divideNumbers = () => 
+{
+    let dividend = parseFloat(document.getElementById("dividend").value);
+    let divisor = parseFloat(document.getElementById("divisor"). value);
+
+    let quotient = divide(dividend, divisor);
+    document.getElementById("quotient").value = quotient;
+}
+document.getElementById("divideNumbers").addEventListener("click" , divideNumbers);
+
 
 
 /* Decision Structure */
 
+function getTotal (){
 
+let subtotal = parseFloat(document.getElementById("subtotal").value);
+if(document.getElementById("number").checked); {
+    subtotal = subtotal - subtotal * 0.2
+}
+document.getElementById("total").textContent = '${subtotal.toFixed(2)}';
+document.getElementById("getTotal").addEventListener("click , getTotal")
+}
 /* ARRAY METHODS - Functional Programming */
+let numbersArray = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+console.log(numbersArray);
+
 /* Output Source Array */
+var arrayElement = document.getElementById("Array");
 
 /* Output Odds Only Array */
+document.querySelector('#odds').innerHTML = numbersArray.filter(number => number % 2 === 0);
 
 /* Output Evens Only Array */
+document.querySelector('#evens').innerHTML = numbersArray.filter(number => number % 2 === 0);
 
 /* Output Sum of Org. Array */
+numbersArray.reduce((sum , number) => sum + number);
+
 
 /* Output Multiplied by 2 Array */
+numbersArray.map(number => number * 2);
+document.getElementById('multiplied').innerHTML = multipliedArray.join (',');
 
 /* Output Sum of Multiplied by 2 Array */
+const multipliedArray = originalArray.map(element => element * 2);
+const sumofMultipliedArray = multipliedArray.reduce((accumulator  , currentValue) => accumulator + currentValue, 0);
+document.getElementById('sumOfMultiplied').textContent = sumOfMultiplied;
+
+
+
+
---
+++ scripts/w04-task.js
@@ -0,0 +1,51 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+
+let myProfile = {
+    name: "Tsitsi Mutsvedu",
+    photo: "images/tsitsi.png",
+    favouriteFoods: [ "rice", "stew", "eggs", "pap", "chips"],
+    hobbies: ["Reading", "Writing", "Hiking"],
+    placesLived: [],
+    myProfile.placesLived.push(
+    {
+        place: 'Zimbabwe',
+        length: '30 years',
+    },
+
+)
+}; 
+
+
+/* Populate Profile Object with placesLive objects */
+
+    myProfile.placesLived.push(
+   {
+
+    place: 'South Africa',
+    length: '10 years',
+
+   }
+
+    )
+
+
+/* DOM Manipulation - Output */
+document.querySelector('#name').textContent = myProfile.name;
+
+/* Name */
+
+/* Photo with attributes */
+ 
+
+/* Favorite Foods List*/
+
+
+
+/* Hobbies List */
+
+
+/* Places Lived DataList */
+
+
