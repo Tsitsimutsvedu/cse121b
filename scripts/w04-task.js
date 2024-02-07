8187d199db4cdcd0710c205098895fdd7d3c4bbf
Tsitsimutsvedu <mutsvedutsitsi@gmail.com>
2024-02-07 16:23:18 +0200

    w04-task.js

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -1,43 +1,56 @@
 /* LESSON 3 - Programming Tasks */
 
 /* Profile Object  */
-    let myProfile = {
+    let myProfile = {};
     myProfile.fullName = "Tsitsi Mutsvedu",
     photo = "images/tsitsi.png",
     favoriteFoods = ["rice", "stew", "eggs", "pap", "chips"],
     hobbies = ["Reading", "Hiking", "Photography"],
-};
+
  /* Populate Profile Object with placesLive objects */
     myProfile.placesLived = [];
     myProfile.placesLived.push({ place: "Zimbabwe", length: "30 years" });
     myProfile.placesLived.push({ place: "South Africa", length: "12 years" });
 // Add more places as needed
 
+
 /* DOM Manipulation - Output */
 /* Name */
     document.getElementById("name").textContent = myProfile.name;
 
 /* Photo with attributes */
+    document.getElementById("name").textContent = myProfile.name;
     document.querySelector('#photo').scr = myProfile.photo.src;
     document.querySelector('#photo').lastChild.myProfile.photo.alt;
 
 
 /* Favorite Foods List*/
-    let favoriteFoodsList = document.getElementById("favorite-foods");
+    favoriteFoodsList = document.getElementById("favorite-foods");
     myProfile.favorite-Foods.forEach((food) => {
-    let listItem = document.createElement("li");
-    listItem.textContent = food;
-    favoriteFoodsList.appendChild(listItem);
+    const li = document.createElement("li");
+    li.textContent = food;
+    favoriteFoodsList.appendChild(li);
+     
 });
     
 /* Hobbies List */
-    hobbies:["Reading", "Writing", "Photography"]
-    myProfile.hobbies.forEach(hobbies => {
-    let ul = document.createElement('ul');
-    ul.textContent = hobbies;
-    document.querySelector('#hobbies').appendChild(ul);
-    })
+    hobbiesList = document.getElementById("hobbies");
+    myProfile.hobbies.forEach((hobby) => {
+    const li = document.createElement("li");
+    li.textContent = hobby;
+    hobbiesList.appendChild(li);
+    });
 
 /* Places Lived DataList */
+    placesLivedList = document.getElementById("places-lived");
+    myProfile.placesLived.forEach((place) => {
+    const dt = document.createElement("dt");
+    dt.textContent = place.place;
+    const dd = document.createElement("dd");
+    dd.textContent = place.length;
+    placesLivedList.appendChild(dt);
+    placesLivedList.appendChild(dd);
+  });
+
 
 
